da14f0582c8cefbfd8740c46f092a6c85f61b26f
Pete Bratach <desertmonkey@users.noreply.github.com>
2018-10-23 17:52:54 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,9 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = [def setup(app):
+    app.add_stylesheet('cumulus.css')
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -130,9 +132,6 @@ html_theme = 'default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-def setup(app):
-    app.add_stylesheet('cumulus.css')
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
